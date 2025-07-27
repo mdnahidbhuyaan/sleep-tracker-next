becf6fe1a578a41a79dada3a0c7e3723dbfe6445
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-07-27 16:43:00 +0600

    add about and contact page hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-
-export default function Home() {
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+export default async function Home() {
+  const  user = await currentUser();
+  if (!user) {
+    return <Guest />; // Render Guest component if no user is signed in
+   
+  }
   return (
 <div>
   <h2 className="text-5xl">Hello nextjs</h2>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -50,5 +50,6 @@ export default async function Navbar() {
         </div>
       </div>
     </nav>
+      
   );
 }
