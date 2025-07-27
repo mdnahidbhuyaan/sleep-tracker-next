8e26ee17f79695721af4595b00668931392776c5
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-07-27 19:29:23 +0600

    Add RecordChart component and integrate with getRecords for sleep data visualization

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AddNewRecord from "@/components/AddNewRecord";
 import Guest from "@/components/Guest";
+import RecordChart from "@/components/RecordChart";
 import { currentUser } from "@clerk/nextjs/server";
 export default async function Home() {
   const  user = await currentUser();
@@ -52,6 +53,7 @@ export default async function Home() {
         {/* Right Column */}
         <div className='space-y-6'>
           {/* Placeholder for RecordStats, RecentRecord, and Insights */}
+          <RecordChart/>
           {/* <RecordChart /> */}
           {/* <AverageSleep /> */}
           {/* <BestWorstSleep /> */}
